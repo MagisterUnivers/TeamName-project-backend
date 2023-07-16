592331f0f428b968bdbf01796da3fa4d1391f56d
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-07-16 22:53:33 +0300

    update RegExp

--- constants/users.js
+++ constants/users.js
@@ -1,7 +1,7 @@
 const emailRegexp =
-/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+/^[a-zA-Z0-9.~+_-]+@[^\s@]+\.[^\s@]+$/;
 const passwordRegexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/;
-const nameRegexp = /^[a-zA-Zа-яА-Я0-9]+$/;
+const nameRegexp = /^[a-zA-Zа-яєїієґҐА-ЯЄЇІЄҐҐ'0-9]+$/;
 
 const themeList = ['dark', 'light'];
 module.exports = { emailRegexp, passwordRegexp, nameRegexp, themeList };

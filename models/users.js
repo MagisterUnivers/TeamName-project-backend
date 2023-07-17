6e3fa837674001d1a9772af78e23151ae10b3b76
Irgey <116591589+Irgey@users.noreply.github.com>
2023-07-17 20:19:08 +0200

    subscription complete

--- models/users.js
+++ models/users.js
@@ -3,42 +3,48 @@ const handleMongooseError = require('../helpers/handleMongooseError');
 const { nameRegexp, emailRegexp, passwordRegexp } = require('../constants/users');
 
 const authSchema = Schema(
-	{
-		name: {
-		type: String,
-		match: nameRegexp,
-		required: [true, 'Name is required']
-	    },
-		email: {
-			type: String,
-			match: emailRegexp,
-			required: [true, 'Email is required'],
-			unique: true
-		},
-		password: {
-			type: String,
-			match: passwordRegexp,
-			minlength: 6,
-			required: [true, 'Set password for user']
-		},
-		avatarURL: {
-			type: String
-		},
-		verify: {
-			type: Boolean,
-			default: false
-		},
-		theme: {
-			type: String,
-			default: 'dark'
-		},
-		verificationToken: {
-			type: String,
-			required: [true, 'Verify token is required']
-		},
-		token: String
-	},
-	{ versionKey: false, timestamps: true }
+  {
+    name: {
+      type: String,
+      match: nameRegexp,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      match: passwordRegexp,
+      minlength: 6,
+      required: [true, "Set password for user"],
+    },
+    avatarURL: {
+      type: String,
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    theme: {
+      type: String,
+      default: "dark",
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
+    token: String,
+    subscriptionEmail: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
 );
 
 authSchema.post('save', handleMongooseError);

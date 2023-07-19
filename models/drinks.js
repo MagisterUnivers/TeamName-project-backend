43f6c025da34910d2b0d74f670bacc28f76800c0
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-07-19 18:16:59 +0300

    FIX: Ingredients type

--- models/drinks.js
+++ models/drinks.js
@@ -1,53 +1,53 @@
-const { Schema, model } = require("mongoose");
-const handleMongooseError = require("../helpers/handleMongooseError");
+const { Schema, model } = require('mongoose');
+const handleMongooseError = require('../helpers/handleMongooseError');
 
-const { categoryList, glassList } = require("../constants/drinks");
+const { categoryList, glassList } = require('../constants/drinks');
 
 const drinksSchema = Schema(
-  {
-    drink: {
-      type: String,
-      required: [true, "Set name for the drink"],
-    },
-    category: {
-      type: String,
-      enum: categoryList,
-      // required: true,
-    },
-    glass: {
-      type: String,
-      enum: glassList,
-      // required: true,
-    },
-    instructions: {
-      type: String,
-      // required: true,
-    },
-    drinkThumb: {
-      type: String,
-    },
-    ingredients: {
-      type: String,
-      // required: true,
-    },
-    about: {
-      type: String,
-      // required: true,
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
-    favorite: {
-      type: [Schema.Types.ObjectId],
-      ref: "user",
-    },
-  },
-  { versionKey: false, timestamps: true }
+	{
+		drink: {
+			type: String,
+			required: [true, 'Set name for the drink']
+		},
+		category: {
+			type: String,
+			enum: categoryList
+			// required: true,
+		},
+		glass: {
+			type: String,
+			enum: glassList
+			// required: true,
+		},
+		instructions: {
+			type: String
+			// required: true,
+		},
+		drinkThumb: {
+			type: String
+		},
+		ingredients: {
+			type: Array
+			// required: true,
+		},
+		about: {
+			type: String
+			// required: true,
+		},
+		owner: {
+			type: Schema.Types.ObjectId,
+			ref: 'user'
+		},
+		favorite: {
+			type: [Schema.Types.ObjectId],
+			ref: 'user'
+		}
+	},
+	{ versionKey: false, timestamps: true }
 );
 
-drinksSchema.post("save", handleMongooseError);
+drinksSchema.post('save', handleMongooseError);
 
-const Drinks = model("cocktail", drinksSchema);
+const Drinks = model('cocktail', drinksSchema);
 
 module.exports = Drinks;

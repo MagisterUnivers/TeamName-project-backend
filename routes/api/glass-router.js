8ad4681e0d78b1546308f716097f1584914e7824
Tanya <tkravche@yahoo.com>
2023-07-20 08:27:58 +0200

    reverse

--- routes/api/glass-router.js
+++ routes/api/glass-router.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const glassController = require('../../controllers/glassController');
-// const authenticate = require('../../middleware/authenticate');
+const authenticate = require('../../middleware/authenticate');
 
 const router = express.Router();
 
-// router.use(authenticate);
+router.use(authenticate);
 
 router.get('/', glassController.getAllGlasses);
 

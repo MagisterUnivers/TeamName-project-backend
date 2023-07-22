b60b9a224cc0705a73226ecd1fc60476718de625
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-07-22 11:48:14 +0200

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -19,128 +19,123 @@ const { saveUserAvatar } = require("../helpers/avatarHelpers");
 //
 
 const register = async (req, res) => {
-	const { email, password } = req.body;
-	const user = await Users.findOne({ email });
-	if (user) throw HttpError(409, 'Email already in use');
-	const hashPassword = await bcrypt.hash(password, 10);
-	const verificationToken = nanoid();
+  const { email, password } = req.body;
+  const user = await Users.findOne({ email });
+  if (user) throw HttpError(409, "Email already in use");
+  const hashPassword = await bcrypt.hash(password, 10);
+  const verificationToken = nanoid();
 
-	const result = await Users.create({
-		...req.body,
-		password: hashPassword,
-		verificationToken
-	});
+  const result = await Users.create({
+    ...req.body,
+    password: hashPassword,
+    verificationToken,
+  });
 
-	const sendEmailToVerify = {
-		to: email,
-		subject: 'Verify email',
-		html: `<a href="http://localhost:3000/TeamName-project/register?verificationToken=${verificationToken}">Click to verify email</a>`
-	};
+  const sendEmailToVerify = {
+    to: email,
+    subject: "Verify email",
+    html: `<a href="http://localhost:3000/TeamName-project/register?verificationToken=${verificationToken}">Click to verify email</a>`,
+  };
 
-	await emailVerify(sendEmailToVerify);
+  await emailVerify(sendEmailToVerify);
 
-	res.status(201).json({
-		user: {
-			name: result.name,
-			email: result.email
-		}
-	});
+  res.status(201).json({
+    user: {
+      name: result.name,
+      email: result.email,
+    },
+  });
 };
 
 const login = async (req, res) => {
-	const { email: userEmail, password } = req.body;
-	const user = await Users.findOne({ email: userEmail });
-	if (!user) throw HttpError(401, 'Email or password is wrong');
-	if (!user.verify) throw HttpError(401, 'Email not register');
+  const { email: userEmail, password } = req.body;
+  const user = await Users.findOne({ email: userEmail });
+  if (!user) throw HttpError(401, "Email or password is wrong");
+  if (!user.verify) throw HttpError(401, "Email not register");
 
-	const passwordCompare = await bcrypt.compare(password, user.password);
-	if (!passwordCompare) throw HttpError(401, 'Email or password is wrong');
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) throw HttpError(401, "Email or password is wrong");
 
-	const { _id: id, email, name } = user;
-	const payload = { id };
+  const { _id: id, email, name } = user;
+  const payload = { id };
 
-	const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
 
-	await Users.findByIdAndUpdate(id, { token });
-	res.json({ token, user: { email, name } });
+  await Users.findByIdAndUpdate(id, { token });
+  res.json({ token, user: { email, name } });
 };
 
 const logout = async (req, res) => {
-	const { _id } = req.user;
+  const { _id } = req.user;
 
-	await Users.findByIdAndUpdate(_id, { token: '' });
+  await Users.findByIdAndUpdate(_id, { token: "" });
 
-	res.status(200).json({
-		message: 'Token has been deleted!'
-	});
+  res.status(200).json({
+    message: "Token has been deleted!",
+  });
 };
 
 const current = async (req, res) => {
-	const { email, _id, name, theme, subscriptionEmail } = req.user;
-	// const { authorization } = req.headers;
-	// const UPDtoken = authorization.split(' ');
+  const { email, _id, name, theme, subscriptionEmail } = req.user;
+  // const { authorization } = req.headers;
+  // const UPDtoken = authorization.split(' ');
 
-	// const user = await Users.findById('64b25f39a99c3c7857e27058');
-	// console.log(user);
-	// const result = await Users.findOne({ token: UPDtoken[1] });
-	// console.log(result);
+  // const user = await Users.findById('64b25f39a99c3c7857e27058');
+  // console.log(user);
+  // const result = await Users.findOne({ token: UPDtoken[1] });
+  // console.log(result);
 
-	// store to redux everything about user
+  // store to redux everything about user
 
-	res.json({
-		email,
-		_id,
-		name,
-		theme,
-		subscriptionEmail
-	});
+  res.json({
+    email,
+    _id,
+    name,
+    theme,
+    subscriptionEmail,
+  });
 };
 
 const refresh = async (req, res) => {
-	const { authorization } = req.headers;
-	const UPDtoken = authorization.split(' ');
-	const user = await Users.findOne({ token: UPDtoken[1] });
-	if (!user)
-		return res
-			.status(401)
-			.json({ error: 'Invalid token. No user with such token' });
-
-	await Users.findByIdAndUpdate(user, { token: UPDtoken[1] });
-	res.json({ token: user.token });
+  const { authorization } = req.headers;
+  const UPDtoken = authorization.split(" ");
+  const user = await Users.findOne({ token: UPDtoken[1] });
+  await Users.findByIdAndUpdate(user, { token: UPDtoken[1] });
+  res.json({ token: UPDtoken[1] });
 };
 
 const subscription = async (req, res) => {
-	const { _id, subscriptionEmail: currentSubscriptionEmail } = req.user;
-	const { email: newSubscriptionEmail } = req.body;
-	// is User with req email exists
-	const user = await Users.findOne({ subscriptionEmail: newSubscriptionEmail });
-	if (user && user._id.toString() !== _id.toString()) {
-		throw HttpError(409, 'Subscription email already in use by another user!');
-	} else if (currentSubscriptionEmail === '') {
-		await Users.findByIdAndUpdate(_id, {
-			subscriptionEmail: newSubscriptionEmail
-		});
-		res.status(200).json({
-			message: 'You successfully subscribed on our newsletter',
-			subscriptionEmail: newSubscriptionEmail
-		});
-	} else {
-		throw HttpError(
-			409,
-			`You already subscribed. Email for newsletter is ${currentSubscriptionEmail}`
-		);
-	}
+  const { _id, subscriptionEmail: currentSubscriptionEmail } = req.user;
+  const { email: newSubscriptionEmail } = req.body;
+  // is User with req email exists
+  const user = await Users.findOne({ subscriptionEmail: newSubscriptionEmail });
+  if (user && user._id.toString() !== _id.toString()) {
+    throw HttpError(409, "Subscription email already in use by another user!");
+  } else if (currentSubscriptionEmail === "") {
+    await Users.findByIdAndUpdate(_id, {
+      subscriptionEmail: newSubscriptionEmail,
+    });
+    res.status(200).json({
+      message: "You successfully subscribed on our newsletter",
+      subscriptionEmail: newSubscriptionEmail,
+    });
+  } else {
+    throw HttpError(
+      409,
+      `You already subscribed. Email for newsletter is ${currentSubscriptionEmail}`
+    );
+  }
 };
 
 const updateTheme = async (req, res) => {
-	const { _id, theme: currentTheme } = req.user;
-	const { theme: newTheme } = req.body;
-	console.log(req.body);
-	if (currentTheme === newTheme) {
-		throw HttpError(400, `${newTheme} theme is already set!`);
-	}
-	await Users.findByIdAndUpdate(_id, { theme: newTheme });
-	res.json({ theme: newTheme });
+  const { _id, theme: currentTheme } = req.user;
+  const { theme: newTheme } = req.body;
+  console.log(req.body);
+  if (currentTheme === newTheme) {
+    throw HttpError(400, `${newTheme} theme is already set!`);
+  }
+  await Users.findByIdAndUpdate(_id, { theme: newTheme });
+  res.json({ theme: newTheme });
 };
 
 // const avatars = async (req, res) => {
@@ -159,66 +154,65 @@ const updateTheme = async (req, res) => {
 //   await req.user.updateOne({ avatarURL });
 //   res.json(req.user.avatarURL);
 // };
-
 const updateUser = async (req, res) => {
-	const { name, avatarURL } = req.body;
-	const updatedFields = {};
+  const { name, avatarURL } = req.body;
+  const updatedFields = {};
 
-	if (avatarURL) {
-		const newAvatarURL = await saveUserAvatar(req, res);
-		updatedFields.avatarURL = newAvatarURL;
-	}
+  if (avatarURL) {
+    const newAvatarURL = await saveUserAvatar(req, res);
+    updatedFields.avatarURL = newAvatarURL;
+  }
 
-	if (name) {
-		updatedFields.name = name;
-	}
+  if (name) {
+    updatedFields.name = name;
+  }
 
-	if (Object.keys(updatedFields).length > 0) {
-		await req.user.updateOne(updatedFields);
-	}
+  if (Object.keys(updatedFields).length > 0) {
+    await req.user.updateOne(updatedFields);
+  }
 
-	res.json(updatedFields);
+  res.json(updatedFields);
 };
 
 const verify = async (req, res) => {
-	const { verificationToken } = req.params;
-	const user = await Users.findOne({ verificationToken });
-	if (!user) throw HttpError(404);
+  const { verificationToken } = req.params;
+  const user = await Users.findOne({ verificationToken });
+  if (!user) throw HttpError(404);
 
-	const { _id: id } = user;
-	const payload = { id };
+  const { _id: id } = user;
+  const payload = { id };
 
-	const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
 
-	await Users.findByIdAndUpdate(user._id, {
-		verify: true,
-		verificationToken: null,
-		token
-	});
+  await Users.findByIdAndUpdate(user._id, {
+    verify: true,
+    verificationToken: null,
+    token,
+  });
 
-	res.status(200).json({
-		message: 'Verification successful',
-		token
-	});
+  res.status(200).json({
+    message: "Verification successful",
+    token,
+  });
 };
 
 const resendVerifyEmail = async (req, res) => {
-	const { email } = req.body;
-	const user = await Users.findOne({ email });
-	if (!user) throw HttpError(401);
-	if (user.verify) throw HttpError(400, 'Verification has already been passed');
+  const { email } = req.body;
+  const user = await Users.findOne({ email });
+  if (!user) throw HttpError(401);
+  if (user.verify) throw HttpError(400, "Verification has already been passed");
 
-	const sendEmailToVerify = {
-		to: email,
-		subject: 'Verify email',
-		html: `<a target="_blank" href="${BASE_URL}/users/verify/${user.verificationToken}">Click to verify email</a>`
-	};
+  const sendEmailToVerify = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/users/verify/${user.verificationToken}">Click to verify email</a>`,
+  };
 
-	await emailVerify(sendEmailToVerify);
+  await emailVerify(sendEmailToVerify);
 
-	res.json({
-		message: 'Verification email sent'
-	});
+  res.json({
+    message: "Verification email sent",
+  });
 };
 
 // CLOUDINARY
